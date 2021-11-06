a76f0794fead36065901a15c17770eef65cb431c
OkkyRoyDirgantara <okkyroydirgantara@gmail.com>
2021-11-07 00:56:45 +0700

    Modified only get API

--- app/Controllers/Posts.php
+++ app/Controllers/Posts.php
@@ -16,75 +16,75 @@ class Posts extends ResourceController
         return $this->respond($this->model->orderBy('id', 'DESC')->findAll(), 200);
     }
 
-    public function create()
-    {
-        if ($this->request) {
-            if ($this->request->getJSON()) {
+    // public function create()
+    // {
+    //     if ($this->request) {
+    //         if ($this->request->getJSON()) {
 
-                $json = $this->request->getJSON();
+    //             $json = $this->request->getJSON();
 
-                $post = $this->model->insert([
-                    'id' => $json->id,
-                    'userId' => $json->userId,
-                    'title'     => $json->title,
-                    'body'   => $json->body
-                ]);
-            } else {
+    //             $post = $this->model->insert([
+    //                 'id' => $json->id,
+    //                 'userId' => $json->userId,
+    //                 'title'     => $json->title,
+    //                 'body'   => $json->body
+    //             ]);
+    //         } else {
 
-                //get request from Postman and more
-                $post = $this->model->insert([
-                    'id' => $this->request->getPost('id'),
-                    'userId' => $this->request->getPost('userId'),
-                    'title'     => $this->request->getPost('title'),
-                    'body'   => $this->request->getPost('body')
-                ]);
-            }
+    //             //get request from Postman and more
+    //             $post = $this->model->insert([
+    //                 'id' => $this->request->getPost('id'),
+    //                 'userId' => $this->request->getPost('userId'),
+    //                 'title'     => $this->request->getPost('title'),
+    //                 'body'   => $this->request->getPost('body')
+    //             ]);
+    //         }
 
-            return $this->respond([
-                'statusCode' => 201,
-                'message'    => 'Data Berhasil Disimpan!',
-            ], 201);
-        }
-    }
+    //         return $this->respond([
+    //             'statusCode' => 201,
+    //             'message'    => 'Data Berhasil Disimpan!',
+    //         ], 201);
+    //     }
+    // }
 
-    public function delete($id = null)
-    {
-        $post = $this->model->find($id);
+    // public function delete($id = null)
+    // {
+    //     $post = $this->model->find($id);
 
-        if ($post) {
+    //     if ($post) {
 
-            $this->model->delete($id);
+    //         $this->model->delete($id);
 
-            return $this->respond([
-                'statusCode' => 200,
-                'message'    => 'Data Berhasil Dihapus!',
-            ], 200);
-        }
-    }
+    //         return $this->respond([
+    //             'statusCode' => 200,
+    //             'message'    => 'Data Berhasil Dihapus!',
+    //         ], 200);
+    //     }
+    // }
 
-    public function update($id = null)
-    {
-        $post = $this->model;
-        if ($this->request->getJSON()) {
+    // public function update($id = null)
+    // {
+    //     $post = $this->model;
+    //     if ($this->request->getJSON()) {
 
-            $json = $this->request->getJSON();
+    //         $json = $this->request->getJSON();
 
-            $post->update($json->id, [
-                'id' => $json->id,
-                'userId' => $json->userId,
-                'title'     => $json->title,
-                'body'   => $json->body
-            ]);
-        } else {
+    //         $post->update($json->id, [
+    //             'id' => $json->id,
+    //             'userId' => $json->userId,
+    //             'title'     => $json->title,
+    //             'body'   => $json->body
+    //         ]);
+    //     } else {
 
-            //get request from Postman and more
-            $data = $this->request->getRawInput();
-            $post->update($id, $data);
-        }
+    //         //get request from Postman and more
+    //         $data = $this->request->getRawInput();
+    //         $post->update($id, $data);
+    //     }
 
-        return $this->respond([
-            'statusCode' => 200,
-            'message'    => 'Data Berhasil Diupdate!',
-        ], 200);
-    }
+    //     return $this->respond([
+    //         'statusCode' => 200,
+    //         'message'    => 'Data Berhasil Diupdate!',
+    //     ], 200);
+    // }
 }
